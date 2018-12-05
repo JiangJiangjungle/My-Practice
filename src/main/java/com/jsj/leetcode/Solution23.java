3fc885d11f6a38884c2640f8deeb32a69649c074
jiangshenjie <741294595@qq.com>
2018-12-05 17:29:32 +0800

    1205 jsj

--- src/main/java/com/jsj/leetcode/Solution23.java
+++ src/main/java/com/jsj/leetcode/Solution23.java
@@ -16,4 +16,46 @@ package com.jsj.leetcode;
  * 输出: 1->1->2->3->4->4->5->6
  */
 public class Solution23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (null == lists || lists.length == 0) return null;
+        ListNode head = null;
+        ListNode last = head;
+        for (int nowIndex = findSmall(lists); nowIndex != -1; nowIndex = findSmall(lists)) {
+            if (head == null) {
+                head = lists[nowIndex];
+                last = head;
+                lists[nowIndex] = lists[nowIndex].next;
+                continue;
+            }
+            last.next = lists[nowIndex];
+            last = lists[nowIndex];
+            lists[nowIndex] = lists[nowIndex].next;
+        }
+        return head;
+    }
+
+    private int findSmall(ListNode[] lists) {
+        int index = -1;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < lists.length; i++) {
+            if (null == lists[i]) continue;
+            if (index == -1 || min > lists[i].val) {
+                index = i;
+                min = lists[i].val;
+            }
+        }
+        return index;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
 }
