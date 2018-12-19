208768567d64aa86789f3fcda5b3614fdc1d995b
jiangshenjie <741294595@qq.com>
2018-12-19 16:23:34 +0800

    1219 jsj

--- src/main/java/com/jsj/leetcode/Solution52.java
+++ src/main/java/com/jsj/leetcode/Solution52.java
@@ -1,5 +1,16 @@
 package com.jsj.leetcode;
 
+/**
+ * @author jsj
+ * @since 2018-12-18
+ * 题目描述：n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * <p>
+ * <p>
+ * <p>
+ * 上图为 8 皇后问题的一种解法。
+ * <p>
+ * 给定一个整数 n，返回 n 皇后不同的解决方案的数量。
+ */
 public class Solution52 {
     public int totalNQueens(int n) {
         int[][] matrixs = new int[n][n];
