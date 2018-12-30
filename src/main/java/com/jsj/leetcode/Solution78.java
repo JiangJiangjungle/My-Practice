c0f59c509718afa16d5cb1a0254466579f9ffca1
jiangshenjie <741294595@qq.com>
2018-12-30 14:54:38 +0800

    1230 jsj

---
+++ src/main/java/com/jsj/leetcode/Solution78.java
@@ -0,0 +1,56 @@
+package com.jsj.leetcode;
+
+import java.util.*;
+
+/**
+ * @author jsj
+ * @since 2018-12-30
+ * 题目描述：给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * <p>
+ * 说明：解集不能包含重复的子集。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ * [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ */
+public class Solution78 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Deque<Integer> deque = new ArrayDeque<>(nums.length);
+        list.add(new ArrayList<>(0));
+        doSubsets(nums, deque, 0, list);
+        return list;
+    }
+
+    private void doSubsets(int[] nums, Deque<Integer> deque, int start, List<List<Integer>> list) {
+        if (start == nums.length) {
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            deque.addLast(nums[i]);
+            list.add(new ArrayList<>(deque));
+            doSubsets(nums, deque, i + 1, list);
+            deque.pollLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> list = new Solution78().subsets(new int[]{1,2,3});
+        if (null!=list){
+            for (List<Integer> aList:list){
+                System.out.println(Arrays.toString(aList.toArray()));
+            }
+        }
+    }
+}
