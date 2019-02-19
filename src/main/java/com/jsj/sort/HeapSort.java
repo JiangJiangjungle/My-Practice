a4da8d0f15eef2590d90824a0a28f6c0d7b224f9
jiangshenjie <741294595@qq.com>
2019-02-19 16:46:38 +0800

    0219 jsj

--- src/main/java/com/jsj/sort/HeapSort.java
+++ src/main/java/com/jsj/sort/HeapSort.java
@@ -12,52 +12,61 @@ import java.util.PriorityQueue;
  */
 public class HeapSort {
 
-    public static int[] sort(int[] array) {
-        if (array == null || array.length == 0) return array;
+    public static int[] sort(int[] arr) {
+        if (arr == null || arr.length == 0) return arr;
         //整理成大顶堆
-        for (int i = array.length / 2 - 1; i >= 0; i--) {
-            adjustHeap(array, i, array.length);
-        }
+        buildMaxHeap(arr);
         //交换堆顶元素与末尾元素，调整堆结构
-        for (int i = array.length - 1; i >= 0; i--) {
-            swap(array, 0, i);
-            adjustHeap(array, 0, i);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            swap(arr, 0, i);
+            adjustHeap(arr, 0, i);
         }
-        return array;
+        return arr;
+    }
+
+    /**
+     * 构造出大顶堆
+     *
+     * @param arr
+     */
+    private static void buildMaxHeap(int[] arr) {
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            adjustHeap(arr, i, arr.length);
+        }
     }
 
     /**
      * 从index开始调整大顶堆（建立在大顶堆已构建的基础上）
      *
-     * @param array
-     * @param index  节点位置
-     * @param length 边界
+     * @param arr
+     * @param i
+     * @param end
      */
-    private static void adjustHeap(int[] array, int index, int length) {
+    private static void adjustHeap(int[] arr, int i, int end) {
         //先取出当前元素i
-        int temp = array[index];
+        int num = arr[i];
         //从i结点的左子结点开始，也就是2i+1处开始
-        for (int k = index * 2 + 1; k < length; k = k * 2 + 1) {
+        for (int k = 2 * i + 1; k < end; k = 2 * k + 1) {
             //如果左子结点小于右子结点，k指向右子结点
-            if (k + 1 < length && array[k] < array[k + 1]) {
+            if (k + 1 < end && arr[k] < arr[k + 1]) {
                 k++;
             }
             //如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
-            if (array[k] > temp) {
-                array[index] = array[k];
-                index = k;
+            if (num < arr[k]) {
+                arr[i] = arr[k];
+                i = k;
             } else {
                 break;
             }
         }
         //将temp值放到最终的位置
-        array[index] = temp;
+        arr[i] = num;
     }
 
-    private static void swap(int[] array, int i, int j) {
-        int tmp = array[i];
-        array[i] = array[j];
-        array[j] = tmp;
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/jsj/sort/MergeSort.java
+++ src/main/java/com/jsj/sort/MergeSort.java
@@ -3,29 +3,38 @@ package com.jsj.sort;
 import java.util.Arrays;
 
 public class MergeSort {
-    public static int[] sort(int[] num) {
-        return doMergeSort(num, 0, num.length);
+
+    public static int[] sort(int[] arr) {
+        doMergeSort(arr, 0, arr.length);
+        return arr;
     }
 
-    private static int[] doMergeSort(int[] num, int start, int end) {
-        if (end - start <= 1) return num;
+    private static void doMergeSort(int[] arr, int start, int end) {
+        if (start >= end - 1) return;
         int mid = (start + end) / 2;
-        doMergeSort(num, start, mid);
-        doMergeSort(num, mid, end);
-        int[] tmp = Arrays.copyOfRange(num, start, end);
+        doMergeSort(arr, start, mid);
+        doMergeSort(arr, mid, end);
+        merge(arr, start, mid, end);
+    }
+
+    private static void merge(int[] arr, int start, int mid, int end) {
+        int[] tmp = new int[end - start];
+        for (int i = 0, j = start; i < tmp.length; i++, j++) {
+            tmp[i] = arr[j];
+        }
         for (int i = 0, j = mid - start, index = start; index < end; index++) {
-            if (i == mid - start || j < tmp.length && tmp[i] > tmp[j]) {
-                num[index] = tmp[j];
-                j++;
-            } else {
-                num[index] = tmp[i];
+            if (j == end - start || i < mid - start && tmp[i] < tmp[j]) {
+                arr[index] = tmp[i];
                 i++;
+            } else {
+                arr[index] = tmp[j];
+                j++;
             }
         }
-        return num;
     }
 
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sort(new int[]{5,4,2,9,3,5})));
+        System.out.println(Arrays.toString(sort(new int[]{5, 4, 2, 9, 3, 5})));
     }
 }
