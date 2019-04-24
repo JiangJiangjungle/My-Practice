740732a352b49538d758168a52ca4db71c902e67
jiangshenjie <741294595@qq.com>
2019-04-24 17:07:34 +0800

    更新

--- src/main/java/com/jsj/leetcode/others/Solution73.java
+++ src/main/java/com/jsj/leetcode/others/Solution73.java
@@ -1,5 +1,9 @@
 package com.jsj.leetcode.others;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author jsj
  * @since 2018-12-27
@@ -41,5 +45,31 @@ package com.jsj.leetcode.others;
  */
 public class Solution73 {
     public void setZeroes(int[][] matrix) {
+        Set<Integer> cols = new HashSet<>();
+        Set<Integer> rows = new HashSet<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    cols.add(i);
+                    rows.add(j);
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (cols.contains(i) || rows.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution73 solution73 = new Solution73();
+        int[][] matrix = new int[][]{{1,1,1},{1,0,1},{1,1,1}};
+        solution73.setZeroes(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
     }
 }
