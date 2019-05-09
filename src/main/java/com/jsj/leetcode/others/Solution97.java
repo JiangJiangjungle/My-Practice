e838be7a1b42ceb6c2ed41122239d9b8442a3398
jiangshenjie <741294595@qq.com>
2019-05-09 14:49:51 +0800

    更新

---
+++ src/main/java/com/jsj/leetcode/others/Solution97.java
@@ -0,0 +1,51 @@
+package com.jsj.leetcode.others;
+
+/**
+ * 给定三个字符串 s1, s2, s3, 验证 s3 是否是由 s1 和 s2 交错组成的。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+ * 输出: false
+ * <p>
+ * 思路：设立3个下标，从左到右逐个尝试（用record缓存中间结果）
+ *
+ * @author jsj
+ * @date 2019-05-09
+ */
+public class Solution97 {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int[][] record = new int[s1.length() + 1][s2.length() + 1];
+        return isInterleave(s1, 0, s2, 0, s3, 0, record);
+    }
+
+    private boolean isInterleave(String s1, int start1, String s2, int start2, String s3, int start3, int[][] record) {
+        boolean interleave = false;
+        if (start3 == s3.length()) {
+            if (start1 == s1.length() && start2 == s2.length()) {
+                interleave = true;
+            }
+        } else {
+            if (start1 < s1.length() && s1.charAt(start1) == s3.charAt(start3)) {
+                if (record[start1 + 1][start2] == 0) {
+                    record[start1 + 1][start2] = isInterleave(s1, start1 + 1, s2, start2, s3, start3 + 1, record)
+                            ? 1 : -1;
+                }
+                interleave = record[start1 + 1][start2] == 1;
+            }
+            if (!interleave && start2 < s2.length() && s2.charAt(start2) == s3.charAt(start3)) {
+                if (record[start1][start2 + 1] == 0) {
+                    record[start1][start2 + 1] = isInterleave(s1, start1, s2, start2 + 1, s3, start3 + 1, record)
+                            ? 1 : -1;
+                }
+                interleave = record[start1][start2 + 1] == 1;
+            }
+        }
+        record[start1][start2] = interleave ? 1 : -1;
+        return interleave;
+    }
+}
---
+++ src/main/java/com/jsj/leetcode/others/Solution98.java
@@ -0,0 +1,79 @@
+package com.jsj.leetcode.others;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 假设一个二叉搜索树具有如下特征：
+ * <p>
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * 2
+ * / \
+ * 1   3
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:
+ * 5
+ * / \
+ * 1   4
+ * / \
+ * 3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ * 根节点的值为 5 ，但是其右子节点值为 4 。
+ * <p>
+ * 思路：中序遍历
+ *
+ * @author jsj
+ * @date 2019-05-09
+ */
+public class Solution98 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        boolean valid = true;
+        if (root == null) {
+            return valid;
+        }
+        boolean started = false;
+        int lastVal = 0;
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.offerLast(root);
+        A:
+        for (; !deque.isEmpty(); ) {
+            for (TreeNode node = deque.pollLast(); node != null; node = node.left) {
+                deque.offerLast(node);
+            }
+            for (TreeNode node = deque.pollLast(); node != null; node = deque.pollLast()) {
+                if (!started) {
+                    started = true;
+                } else if (node.val <= lastVal) {
+                    valid = false;
+                    break A;
+                }
+                lastVal = node.val;
+                if (node.right != null) {
+                    deque.offerLast(node.right);
+                    break;
+                }
+            }
+        }
+        return valid;
+    }
+}
