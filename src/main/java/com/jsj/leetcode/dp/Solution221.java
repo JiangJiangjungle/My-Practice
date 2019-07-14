8ea739381ec5c0b6248d7970380933ec09d73340
jiangshenjie <741294595@qq.com>
2019-07-14 20:52:52 +0800

    更新

---
+++ src/main/java/com/jsj/leetcode/dp/Solution221.java
@@ -0,0 +1,39 @@
+package com.jsj.leetcode.dp;
+
+/**
+ * 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入:
+ * <p>
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ * <p>
+ * 输出: 4
+ *
+ * <p>
+ * 思路：dp[i][j]表示从(0，0)点到(i，j)点包含的最大正方形边长，从0,0开始遍历
+ * if dp[i][j]==1 则有 dp[i][j]=Min{dp[i-1][j],dp[i][j-1],dp[i-1][j-1]}+1
+ *
+ * @author jsj
+ * @date 2019-07-14
+ */
+public class Solution221 {
+    public int maximalSquare(char[][] matrix) {
+        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int maxsqlen = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    maxsqlen = Math.max(maxsqlen, dp[i][j]);
+                }
+            }
+        }
+        return maxsqlen * maxsqlen;
+    }
+}
